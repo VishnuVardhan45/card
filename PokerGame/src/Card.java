05a05e8f1865976d219155352d215607a43fdc89
vishnuvardhan <Vishnuvardhan.Yeddula@cdk.com>
2016-07-07 18:06:25 +0530

    added toString method

--- PokerGame/src/Card.java
+++ PokerGame/src/Card.java
@@ -1,4 +1,14 @@
 
 public class Card {
-
+	private int suit;
+	private int pip;
+	public Card(int suit,int pip) {
+		this.suit = suit;
+		this.pip = pip;
+	}
+	public String toString() {
+		// TODO Auto-generated method stub
+		return suit+""+pip;
+		
+	}
 }

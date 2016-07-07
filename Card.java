15ba6adde4fa3fd1f9a22153ce474b9f2c45f25f
vishnuvardhan <Vishnuvardhan.Yeddula@cdk.com>
2016-07-07 20:01:14 +0530

    added toString method

--- Card.java
+++ Card.java
@@ -1,14 +1,20 @@
+
 public class Card {
-	public void card(int pip,int suit) {
+	private int suit;
+	private int pip;
+	public Card(int suit,int pip) {
+		this.suit = suit;
 		this.pip = pip;
-		this.suit = suit;			
 	}
-	public void getSuit() {
-		String suits [] = {"c","d","h","s"};
-		for(int i = 0 ; i < 4 ; i++) {
-			if(suits[i] == suit) {
-			return i;
-			}
-		}	
+	public int getSuit() {
+		return suit;
+	}
+	public int getPip() {
+		return pip;
+	}
+	public String toString() {
+		// TODO Auto-generated method stub
+		return suit+""+pip;
+		
 	}
 }

2392ff2b4504087ead4a6ac5bee1a968a298309d
Shanthi <shanthipriya1015@gmail.com>
2016-07-07 19:46:08 +0530

    added main class

---
+++ pokerhand.java
@@ -0,0 +1,17 @@
+
+public class pokerhand {
+
+	
+	public static void main(String[] args) {
+	
+	int a[]= {34,50,3,45,21};
+		
+card c1= new card(a);
+
+System.out.print(c1.pokerhand());
+
+
+	}
+
+}
+

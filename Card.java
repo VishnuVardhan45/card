22532990da7ddb529456a4abea0df2a58d91175c
vishnuvardhan <Vishnuvardhan.Yeddula@cdk.com>
2016-07-07 15:08:23 +0530

    Added get suit method

--- Card.java
+++ Card.java
@@ -3,5 +3,12 @@ public class Card {
 		this.pip = pip;
 		this.suit = suit;			
 	}
-
+	public void getSuit() {
+		String suits [] = {"c","d","h","s"};
+		for(int i = 0 ; i < 4 ; i++) {
+			if(suits[i] == suit) {
+			return i;
+			}
+		}	
+	}
 }

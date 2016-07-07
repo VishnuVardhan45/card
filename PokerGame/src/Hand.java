eae3b7895d6f14a1fed33d8f12070e330cac1f15
vishnuvardhan <Vishnuvardhan.Yeddula@cdk.com>
2016-07-07 19:02:25 +0530

    added sortByRank method

--- PokerGame/src/Hand.java
+++ PokerGame/src/Hand.java
@@ -1,7 +1,10 @@
 
 public class Hand {
 	public String playerHandRanking(Card cards[]) {
-		System.out.println(isFlush(cards));
+		if(isFlush(cards)) {
+			return "Player Hand Ranking is Flush";
+		}
+		
 		return "something";
 	}
 	public static void sortBySuit( Card[] c )
@@ -25,4 +28,22 @@ public class Hand {
 		sortBySuit(cards);
  		return cards[0].getSuit() == cards[4].getSuit();
 	}
+	 public static void sortByRank( Card[] c )
+	   {
+	      int i, j, min_j;
+	      for ( i = 0 ; i < c.length ; i ++ )
+	      {
+	         min_j = i;   	     
+	      for ( j = i+1 ; j < c.length ; j++ )
+	         {
+	            if ( c[j].getPip() < c[min_j].getPip() )
+	            {
+	               min_j = j;         
+	            }
+	         }
+	      	 Card help = c[i];
+	         c[i] = c[min_j];
+	         c[min_j] = help;
+	      }
+	   }
 }

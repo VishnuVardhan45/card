bf45ee87ffe911a6afacb0e0f3d88c59b140a8d5
yessi <yeshwant.yessssi@gmail.com>
2016-07-08 09:15:35 +0530

    ADDED POKERTEST CLASS

---
+++ testPokerhand.java
@@ -0,0 +1,17 @@
+
+public class pokerhand {
+
+	
+	public static void main(String[] args) {
+	
+	int a[]= {34,50,3,45,21};
+		
+cards c1= new cards(a);
+
+System.out.print(c1.pokerhand());
+
+
+	}
+
+}
+

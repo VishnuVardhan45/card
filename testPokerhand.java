86f052dacb85e8c7034435e63713ba0ef306849e
Shanthi <shanthipriya1015@gmail.com>
2016-07-08 11:04:09 +0530

    added 2nd player

--- testPokerhand.java
+++ testPokerhand.java
@@ -1,16 +1,28 @@
+public class cardGame {
 
-public class testPokerhand {
-
-	
 	public static void main(String[] args) {
-	
-	int a[]= {34,50,3,45,21};
-		PokerHand c1= new PokerHand(a);
 
-System.out.print(c1.pokerhand());
+		int a[] = { 5, 14, 2, 3, 4 };
+		int b[] = { 15, 46, 22, 31, 4 };
 
+		PokerHand c1 = new PokerHand(a);
+
+		String pokerhand1 = c1.pokerhand();
+
+		System.out.println("player1   " + pokerhand1 + "  ");
+		int player1rank = c1.getRank();
+
+		PokerHand c2 = new PokerHand(b);
+		String pokerhand2 = c2.pokerhand();
+		System.out.println("player2   " + pokerhand2 + "  ");
+		int player2rank = c2.getRank();
+
+		if (player1rank < player2rank) {
+			System.out.println("player1 is winner");
+		} else {
+			System.out.println("player2 is winner");
+		}
 
 	}
-
 }
 

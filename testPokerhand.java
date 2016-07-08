b89c53db97f53ac36c6625dc52380d8f048ba74f
Shanthi <shanthipriya1015@gmail.com>
2016-07-08 10:42:48 +0530

    changed

--- testPokerhand.java
+++ testPokerhand.java
@@ -1,12 +1,11 @@
 
-public class pokerhand {
+public class testPokerhand {
 
 	
 	public static void main(String[] args) {
 	
 	int a[]= {34,50,3,45,21};
-		
-cards c1= new cards(a);
+		PokerHand c1= new PokerHand(a);
 
 System.out.print(c1.pokerhand());
 
